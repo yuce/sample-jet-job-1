43769a0f1ac4f335582ff76922b1788ca2a158e3
Yuce Tekol <yuce.tekol@hazelcast.com>
2023-04-16 20:45:01 +0300

    Updated the sample to use the Hashids library

--- src/main/java/clc/example/JetJob.java
+++ src/main/java/clc/example/JetJob.java
@@ -4,17 +4,15 @@ import com.hazelcast.core.Hazelcast;
 import com.hazelcast.jet.pipeline.Pipeline;
 import com.hazelcast.jet.pipeline.Sinks;
 import com.hazelcast.jet.pipeline.test.TestSources;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import java.io.IOException;
+import org.hashids.Hashids;
 
 public class JetJob {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        final var salt = (args.length > 0)? args[0] : "my-salt";
         var pipeline = Pipeline.create();
-        pipeline.readFrom(TestSources.items(args))
-                .map(url -> Jsoup.connect(url).get().title())
-                .setLocalParallelism(1)
+        pipeline.readFrom(TestSources.itemStream(1))
+                .withoutTimestamps()
+                .map(e -> new Hashids(salt).encode(e.sequence()))
                 .writeTo(Sinks.logger());
         var hz = Hazelcast.bootstrappedInstance();
         hz.getJet().newJob(pipeline);
